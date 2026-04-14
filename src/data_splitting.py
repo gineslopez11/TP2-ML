75768312843b1899f96747769dcb578c1332a2b7
gineslopez11 <glopezpena@udesa.edu.ar>
2026-04-14 19:56:23 -0300

    Cross_val funcionaando

--- src/data_splitting.py
+++ src/data_splitting.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pandas as pd
+from src.models import LogisticRegression
+from src.metrics import F1_score
+from src.preprocessing import *
 
 def random_split(dev,percentage,rand_state = None):
 	dev_insuficiente = dev[dev['rendimiento_binario'] == 0]
@@ -44,3 +47,47 @@ def temporal_split(dev,m):
 	test = dev[dev['semestre'].isin(restantes)]
 
 	return train, test
+
+def cross_val(dev, nombres_features, y_col, K, L2, alfa, iters, umbral, group_key, columnas_continuas, tipo,clase_positiva):
+	n = len(dev)  
+	
+	if tipo == 'aleatorio':
+		indices = np.arange(n)
+		np.random.shuffle(indices)
+		folds = np.array_split(indices, K)
+
+	elif tipo == 'group':
+		grupos = dev[group_key].unique()
+		folds = [np.where(dev[group_key] == g)[0] for g in grupos]
+
+	elif tipo == 'temporal':
+		semestres = sorted(dev['semestre'].unique(), key=lambda x: (x.split('-')[0], x.split('-')[1]))
+		folds = [np.where(dev['semestre'] == s)[0] for s in semestres]
+
+	F1s = []
+
+	for i in range(len(folds)):
+		val_idx = folds[i]
+		train_idx = np.concatenate([folds[j] for j in range(len(folds)) if j != i])
+
+		train_fold = dev.iloc[train_idx].copy()
+		val_fold = dev.iloc[val_idx].copy()
+
+		reemplazo_NaNs(train_fold, val_fold, group_key, columnas_continuas)
+		train_fold_norm, val_fold_norm, _ = normalizar(train_fold, val_fold, columnas_continuas)
+
+		X_train_fold = train_fold_norm[nombres_features].values
+		y_train_fold = train_fold_norm[y_col].values
+		X_val_fold = val_fold_norm[nombres_features].values
+		y_val_fold = val_fold_norm[y_col].values
+
+		modelo_train = LogisticRegression(X_train_fold, y_train_fold, nombres_features, L2)
+		modelo_train.entrenar_gradiente_descendiente(alfa, iters)
+
+		y_pred_prob = modelo_train.predecir(X_val_fold)
+		y_pred_clase = modelo_train.predecir_clase(y_pred_prob, umbral)
+		F1_i = F1_score(y_pred_clase, y_val_fold, clase_positiva)
+
+		F1s.append(F1_i)
+
+	return np.mean(F1s)  
